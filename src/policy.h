bce1546666896d28a8efb28800616e14d67fc123
Luke Dashjr <luke-jr+git@utopios.org>
2014-10-10 03:12:12 +0000

    Introduce CNodePolicy for putting isolated node policy code and parameters on
    Initially populated with policy-specific code moved from AcceptToMemoryPool.

--- src/policy.h
+++ src/policy.h
@@ -8,9 +8,36 @@
 
 class CFeeRate;
 class CTxOut;
+class CCoinsViewCache;
+class CTransaction;
+class CTxMemPool;
+class CTxMemPoolEntry;
+class CValidationState;
 
 extern CFeeRate minRelayTxFee;
 
 bool IsDust(const CTxOut& txout);
+
+class CNodePolicyBase
+{
+public:
+    virtual bool AcceptTxPoolPreInputs(CTxMemPool&, CValidationState&, const CTransaction&) = 0;
+    virtual bool AcceptTxWithInputs(CTxMemPool&, CValidationState&, const CTransaction&, CCoinsViewCache&) = 0;
+    virtual bool AcceptMemPoolEntry(CTxMemPool&, CValidationState&, CTxMemPoolEntry&, CCoinsViewCache&, bool& fRateLimit) = 0;
+    virtual bool RateLimitTx(CTxMemPool&, CValidationState&, CTxMemPoolEntry&, CCoinsViewCache&) = 0;
+};
+
+class CNodePolicy : CNodePolicyBase
+{
+public:
+    bool fRequireStandardTx;
+
+    CNodePolicy() : fRequireStandardTx(true) { };
+
+    virtual bool AcceptTxPoolPreInputs(CTxMemPool&, CValidationState&, const CTransaction&);
+    virtual bool AcceptTxWithInputs(CTxMemPool&, CValidationState&, const CTransaction&, CCoinsViewCache&);
+    virtual bool AcceptMemPoolEntry(CTxMemPool&, CValidationState&, CTxMemPoolEntry&, CCoinsViewCache&, bool& fRateLimit);
+    virtual bool RateLimitTx(CTxMemPool&, CValidationState&, CTxMemPoolEntry&, CCoinsViewCache&);
+};
 
 #endif // BITCOIN_POLICY_H
