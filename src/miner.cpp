55e78b0eeb28260519c44a1571b45557a2438243
jtimon <jtimon@jtimon.cc>
2014-12-18 22:52:08 +0100

    Refactor: miner.cpp: Move local variable to the smaller scope where its needed

--- src/miner.cpp
+++ src/miner.cpp
@@ -156,9 +156,6 @@ CBlockTemplate* CreateNewBlock(const CPolicy& policy, const CChainParams& chainp
     unsigned int nBlockMinSize = GetArg("-blockminsize", DEFAULT_BLOCK_MIN_SIZE);
     nBlockMinSize = std::min(nBlockMaxSize, nBlockMinSize);
 
-    // Collect memory pool transactions into the block
-    CAmount nFees = 0;
-
     {
         LOCK2(cs_main, mempool.cs);
         CBlockIndex* pindexPrev = chainActive.Tip();
@@ -245,7 +242,8 @@ CBlockTemplate* CreateNewBlock(const CPolicy& policy, const CChainParams& chainp
                 vecPriority.push_back(TxPriority(dPriority, feeRate, &mi->second.GetTx()));
         }
 
-        // Collect transactions into block
+        // Collect memory pool transactions into the block
+        CAmount nFees = 0;
         uint64_t nBlockSize = 1000;
         uint64_t nBlockTx = 0;
         int nBlockSigOps = 100;
