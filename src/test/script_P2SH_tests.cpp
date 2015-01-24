8ed3f644695befa84aa4e65e95f910af72c89218
jtimon <jtimon@jtimon.cc>
2015-01-24 17:15:49 +0100

    Policy: Refactor: Create CPolicy::ApproveTx(tx, state) from main::IsStandardTx(tx, reason)

--- src/test/script_P2SH_tests.cpp
+++ src/test/script_P2SH_tests.cpp
@@ -5,6 +5,7 @@
 #include "coins.h"
 #include "coinscache.h"
 #include "consensus/consensus.h"
+#include "consensus/validation.h"
 #include "key.h"
 #include "keystore.h"
 #include "main.h"
@@ -86,7 +87,7 @@ BOOST_AUTO_TEST_CASE(sign)
     }
 
     CMutableTransaction txFrom;  // Funding transaction:
-    string reason;
+    CValidationState state;
     txFrom.vout.resize(8);
     for (int i = 0; i < 4; i++)
     {
@@ -95,7 +96,7 @@ BOOST_AUTO_TEST_CASE(sign)
         txFrom.vout[i+4].scriptPubKey = standardScripts[i];
         txFrom.vout[i+4].nValue = COIN;
     }
-    BOOST_CHECK(IsStandardTx(txFrom, reason));
+    BOOST_CHECK(Policy("standard").ApproveTx(txFrom, state));
 
     CMutableTransaction txTo[8]; // Spending transactions
     for (int i = 0; i < 8; i++)
@@ -185,14 +186,14 @@ BOOST_AUTO_TEST_CASE(set)
     }
 
     CMutableTransaction txFrom;  // Funding transaction:
-    string reason;
+    CValidationState state;
     txFrom.vout.resize(4);
     for (int i = 0; i < 4; i++)
     {
         txFrom.vout[i].scriptPubKey = outer[i];
         txFrom.vout[i].nValue = CENT;
     }
-    BOOST_CHECK(IsStandardTx(txFrom, reason));
+    BOOST_CHECK(Policy("standard").ApproveTx(txFrom, state));
 
     CMutableTransaction txTo[4]; // Spending transactions
     for (int i = 0; i < 4; i++)
@@ -210,7 +211,7 @@ BOOST_AUTO_TEST_CASE(set)
     for (int i = 0; i < 4; i++)
     {
         BOOST_CHECK_MESSAGE(SignSignature(keystore, txFrom, txTo[i], 0), strprintf("SignSignature %d", i));
-        BOOST_CHECK_MESSAGE(IsStandardTx(txTo[i], reason), strprintf("txTo[%d].IsStandard", i));
+        BOOST_CHECK_MESSAGE(Policy("standard").ApproveTx(txTo[i], state), strprintf("txTo[%d].IsStandard", i));
     }
 }
 
