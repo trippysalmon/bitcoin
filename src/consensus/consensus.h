a7c64fdadf5a4c33fc408b7aa9f2de4cd7966a12
jtimon <jtimon@jtimon.cc>
2015-01-24 16:38:19 +0100

    Consensus: SQUASHME: MOVEONLY: Consensus::CheckTx() to consensus.o

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -19,7 +19,7 @@ static const int COINBASE_MATURITY = 100;
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
-/** 
+/**
  * Consensus validations:
  * Check_ means checking everything possible with the data provided.
  * Verify_ means all data provided was enough for this level and its "consensus-verified".
