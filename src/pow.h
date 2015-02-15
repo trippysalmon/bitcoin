fde907d73d5ce9ecaad0536982fb207dc34cb295
Jorge Timón <jtimon@jtimon.cc>
2015-02-15 02:21:42 +0100

    Consensus: Refactor: Decouple pow.o from chainparams.o

--- src/pow.h
+++ src/pow.h
@@ -6,6 +6,8 @@
 #ifndef BITCOIN_POW_H
 #define BITCOIN_POW_H
 
+#include "consensus/params.h"
+
 #include <stdint.h>
 
 class CBlockHeader;
@@ -13,10 +15,10 @@ class CBlockIndex;
 class uint256;
 class arith_uint256;
 
-unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock);
+unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
-bool CheckProofOfWork(uint256 hash, unsigned int nBits);
+bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params);
 arith_uint256 GetBlockProof(const CBlockIndex& block);
 
 #endif // BITCOIN_POW_H
