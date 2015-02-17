8583b0daa0b86363e995ebb25e0ee6a71d16c4f4
Jorge Timón <jtimon@jtimon.cc>
2015-02-17 03:39:11 +0100

    Consensus: MOVEONLY: GetP2SHSigOpCount() from main to consensus

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -64,6 +64,13 @@ bool CheckTxInputsScripts(const CTransaction& tx, CValidationState& state, const
  * @see CTransaction::FetchInputs
  */
 unsigned int GetLegacySigOpCount(const CTransaction& tx);
+/**
+ * Count ECDSA signature operations in pay-to-script-hash inputs.
+ * @param[in] mapInputs Map of previous transactions that have outputs we're spending
+ * @return maximum number of sigops required to validate this transaction's inputs
+ * @see CTransaction::FetchInputs
+ */
+unsigned int GetP2SHSigOpCount(const CTransaction& tx, const CCoinsViewEfficient& mapInputs);
 CAmount GetBlockValue(int nHeight, const Consensus::Params& params, const CAmount& nFees);
 
 } // namespace Consensus
