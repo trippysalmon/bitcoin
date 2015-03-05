324e5519ff9d85d6274bf8aac352eb7610f74a14
Jorge Timón <jtimon@jtimon.cc>
2015-03-05 19:18:45 +0100

    Policy: Refactor: Simplify CheckInputsScripts

--- src/main.cpp
+++ src/main.cpp
@@ -1133,26 +1133,16 @@ int GetSpendHeight(const CCoinsViewCache& inputs)
 
 bool CheckInputsScripts(const CTransaction& tx, CValidationState& state, const CCoinsViewCache& inputs, unsigned int flags, bool cacheStore)
 {
-            for (unsigned int i = 0; i < tx.vin.size(); i++) {
-                const COutPoint &prevout = tx.vin[i].prevout;
-                const CCoins* coins = inputs.AccessCoins(prevout.hash);
-                assert(coins);
-
-                // Verify signature
-                CScriptCheck check(*coins, tx, i, flags, cacheStore);
-                if (!check()) {
-                    if (flags & STANDARD_NOT_MANDATORY_VERIFY_FLAGS) {
+    if (!Consensus::CheckTxInputsScripts(tx, state, inputs, true, flags)) {
                         // Check whether the failure was caused by a
                         // non-mandatory script verification check, such as
                         // non-standard DER encodings or non-null dummy
                         // arguments; if so, don't trigger DoS protection to
                         // avoid splitting the network between upgraded and
                         // non-upgraded nodes.
-                        CScriptCheck check(*coins, tx, i,
-                                flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS, cacheStore);
-                        if (check())
-                            return state.Invalid(false, REJECT_NONSTANDARD, strprintf("non-mandatory-script-verify-flag (%s)", ScriptErrorString(check.GetScriptError())));
-                    }
+        if ((flags & STANDARD_NOT_MANDATORY_VERIFY_FLAGS) &&
+            Consensus::CheckTxInputsScripts(tx, state, inputs, true, flags & ~STANDARD_NOT_MANDATORY_VERIFY_FLAGS))
+            return state.Invalid(false, REJECT_NONSTANDARD, strprintf("with flags: STANDARD_NOT_MANDATORY (%s)", state.GetRejectReason()));
                     // Failures of other flags indicate a transaction that is
                     // invalid in new blocks, e.g. a invalid P2SH. We DoS ban
                     // such nodes as they are not following the protocol. That
@@ -1160,10 +1150,8 @@ bool CheckInputsScripts(const CTransaction& tx, CValidationState& state, const C
                     // as to the correct behavior - we may want to continue
                     // peering with non-upgraded nodes even after a soft-fork
                     // super-majority vote has passed.
-                    return state.DoS(100,false, REJECT_INVALID, strprintf("mandatory-script-verify-flag-failed (%s)", ScriptErrorString(check.GetScriptError())));
-                }
-            }
-
+        return state.DoS(100,false, REJECT_INVALID, strprintf("with flags: MANDATORY (%s)", state.GetRejectReason()));
+    }
     return true;
 }
 
