8d7fe5091482e7562697444aa022ddd7b7f14975
Jorge Timón <jtimon@jtimon.cc>
2015-04-22 15:53:59 +0200

    Consensus: Refactor: Decouple CheckTransaction from util.o and boost

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -371,8 +371,10 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
             CWalletTx wtx;
             ssValue >> wtx;
             CValidationState state;
-            if (!(CheckTransaction(wtx, state) && (wtx.GetHash() == hash) && state.IsValid()))
+            if (!(CheckTransaction(wtx, state) && (wtx.GetHash() == hash) && state.IsValid())) {
+                strErr = strprintf("%s: CheckTransaction: ", __func__, state.GetRejectReason().c_str());
                 return false;
+            }
 
             // Undo serialize changes in 31600
             if (31404 <= wtx.fTimeReceivedIsTxTime && wtx.fTimeReceivedIsTxTime <= 31703)
