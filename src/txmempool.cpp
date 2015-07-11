d36e3225cb20d28fa6e2d0357057a8d9c2e64c6a
Jorge Timón <jtimon@jtimon.cc>
2015-07-11 16:40:56 +0200

    fixup! We don't know what the score may be based on in the future

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -32,7 +32,7 @@ CTxMemPoolEntry::CTxMemPoolEntry(const CTransaction& _tx, const CAmount& _nFee,
     nTxSize = ::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION);
     nModSize = tx.CalculateModifiedSize(nTxSize);
     nUsageSize = tx.DynamicMemoryUsage();
-    feeRate = CFeeRate(nFee, nTxSize);
+    nScore = nFee / nTxSize;
 }
 
 CTxMemPoolEntry::CTxMemPoolEntry(const CTxMemPoolEntry& other)
