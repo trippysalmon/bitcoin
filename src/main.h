39595f6f8cd921d2680498c18158c547a0fa2709
Jorge Timón <jtimon@jtimon.cc>
2015-07-11 23:17:50 +0200

    Policy: Turn policy globals into CStandardPolicy attributes
    ...by separating CStandardPolicy implementation from CPolicy interface
    
    Eliminated globals:
    
    -fIsBareMultisigStd
    -fRequireStandard

--- src/main.h
+++ src/main.h
@@ -42,6 +42,7 @@ class CBloomFilter;
 class CInv;
 class CPolicy;
 class CScriptCheck;
+class CStandardPolicy;
 class CValidationInterface;
 class CValidationState;
 
@@ -99,14 +100,12 @@ extern bool fImporting;
 extern bool fReindex;
 extern int nScriptCheckThreads;
 extern bool fTxIndex;
-extern bool fIsBareMultisigStd;
-extern bool fRequireStandard;
 extern bool fCheckBlockIndex;
 extern bool fCheckpointsEnabled;
 extern size_t nCoinCacheUsage;
 extern CFeeRate minRelayTxFee;
 extern bool fAlerts;
-extern CPolicy globalPolicy;
+extern CStandardPolicy globalPolicy;
 
 /** Best header we've seen so far (used for getheaders queries' starting points). */
 extern CBlockIndex *pindexBestHeader;
