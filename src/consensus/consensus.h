46a520ab676fe97a597a55df2f79d694352f8599
Jorge Timón <jtimon@jtimon.cc>
2015-08-27 00:35:30 +0200

    Consensus: Decouple consensus/consensus from chainparams and timedata
    Chainparams: Explicit Consensus::Params for consensus functions:
    
    -CheckBlockHeader
    -ContextualCheckBlockHeader
    -CheckBlock
    -ContextualCheckBlock
    
    Also add nTime parameter to CheckBlockHeader and CheckBlock.
    Also use the oportunity to rename the functions inside the Consensus namespace.

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -59,31 +59,31 @@ bool CheckTxInputs(const CTransaction& tx, CValidationState& state, const CCoins
  */
 bool VerifyTx(const CTransaction& tx, CValidationState& state, const unsigned int flags, const int nHeight, const int64_t nMedianTimePast, const int64_t nBlockTime, int64_t& nSigOps);
 
-} // namespace Consensus
-
 /** Block Header validation functions */
 
 /**
  * Context-independent CBlockHeader validity checks
  */
-bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, bool fCheckPOW = true);
+bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, const Params& consensusParams, int64_t nTime, bool fCheckPOW = true);
 /**
  * Context-dependent CBlockHeader validity checks.
  * By "context", we mean only the previous block headers, but not the UTXO set.
  * UTXO-related validity checks are still done in main::ConnectBlock().
  */
-bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& state, CBlockIndex* pindexPrev);
+bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& state, const Params& consensusParams, const CBlockIndex* pindexPrev);
 
 /** Block validation functions */
 
 /**
  * Context-independent CBlock validity checks
  */
-bool CheckBlock(const CBlock& block, CValidationState& state, bool fCheckPOW = true, bool fCheckMerkleRoot = true);
+bool CheckBlock(const CBlock& block, CValidationState& state, const Params& consensusParams, int64_t nTime, bool fCheckPOW = true, bool fCheckMerkleRoot = true);
 /**
  * Context-dependent CBlock validity checks
  */
-bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIndex* pindexPrev);
+bool ContextualCheckBlock(const CBlock& block, CValidationState& state, const Params& consensusParams, const CBlockIndex* pindexPrev);
+
+} // namespace Consensus
 
 /** Transaction validation utility functions */
 
