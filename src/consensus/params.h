be24f6457440b9235cb6d5ccf5344f28842b3ab1
Jorge Timón <jtimon@jtimon.cc>
2015-10-24 16:46:21 +0200

    Consensus: Prepare consensus/params for versionbits

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -8,7 +8,60 @@
 
 #include "uint256.h"
 
+#define VERSIONBITS_UNIT_TEST
+
 namespace Consensus {
+
+enum Rule
+{
+#ifdef VERSIONBITS_UNIT_TEST
+    TEST1,
+    TEST2,
+    TEST3,
+    TEST4,
+    TEST5,
+    TEST6,
+    TEST7,
+    TEST8,
+    TEST9,
+    TEST10,
+    TEST11,
+    TEST12,
+    TEST13,
+    TEST14,
+    TEST15,
+    TEST16,
+    TEST17,
+    TEST18,
+    TEST19,
+    TEST20,
+    TEST21,
+    TEST22,
+    TEST23,
+    TEST24,
+    TEST25,
+    TEST26,
+    TEST27,
+    TEST28,
+    TEST29,
+    TEST30,
+#endif
+    MAX_VERSION_BITS_DEPLOYMENTS,
+    // Old style deployments:
+    BIP16,
+    BIP30,
+    BIP34,
+    BIP65,
+    BIP66
+};
+
+struct SoftFork
+{
+    int32_t nBit;
+    uint32_t nDeployTime;
+    uint32_t nExpireTime;
+};
+
 /**
  * Parameters that influence chain consensus.
  */
@@ -19,6 +72,9 @@ struct Params {
     int nMajorityEnforceBlockUpgrade;
     int nMajorityRejectBlockOutdated;
     int nMajorityWindow;
+    /** Used for soft fork deployments using versionbits */
+    int64_t nRuleChangeActivationThreshold;
+    SoftFork vDeployments[MAX_VERSION_BITS_DEPLOYMENTS];
     /** Proof of work parameters */
     uint256 powLimit;
     bool fPowAllowMinDifficultyBlocks;
