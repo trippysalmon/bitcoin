d3cf04f547d8d2e6eefc38858dd931a4985955f0
Jorge Timón <jtimon@jtimon.cc>
2015-12-28 17:50:29 +0100

    Consensus: Move BIP30 code to GetConsensusFlags and Consensus::VerifyTx

--- src/consensus/consensus.cpp
+++ src/consensus/consensus.cpp
@@ -34,7 +34,7 @@ static bool IsSuperMajority(int minVersion, const CBlockIndexView* pstart, unsig
     return (nFound >= nRequired);
 }
 
-unsigned int GetConsensusFlags(const CBlockHeader& block, const Consensus::Params& consensusParams, const CBlockIndexView* pindex)
+unsigned int GetConsensusFlags(const CBlockHeader& block, const Consensus::Params& consensusParams, const CBlockIndexView* pindex, bool fNewBlock)
 {
     // BIP16 didn't become active until Apr 1 2012
     bool fStrictPayToScriptHash = pindex->GetTime() >= 1333238400;
@@ -53,6 +53,34 @@ unsigned int GetConsensusFlags(const CBlockHeader& block, const Consensus::Param
     // Start enforcing CHECKLOCKTIMEVERIFY, (BIP65) for block.nVersion=4
     if (block.nVersion >= 4 && IsSuperMajority(4, pindex->GetPrev(), consensusParams.nMajorityEnforceBlockUpgrade, consensusParams))
         flags |= SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY;
+
+    // Do not allow blocks that contain transactions which 'overwrite' older transactions,
+    // unless those are already completely spent.
+    // If such overwrites are allowed, coinbases and transactions depending upon those
+    // can be duplicated to remove the ability to spend the first instance -- even after
+    // being sent to another address.
+    // See BIP30 and http://r6.ca/blog/20120206T005236Z.html for more information.
+    // This logic is not necessary for memory pool transactions, as AcceptToMemoryPool
+    // already refuses previously-known transaction ids entirely.
+    // This rule was originally applied to all blocks with a timestamp after March 15, 2012, 0:00 UTC.
+    // Now that the whole chain is irreversibly beyond that time it is applied to all blocks except the
+    // two in the chain that violate it. This prevents exploiting the issue against nodes during their
+    // initial block download.
+    bool fEnforceBIP30 = !((pindex->GetHeight()==91842 && pindex->GetBlockHash() == uint256S("0x00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec")) ||
+          (pindex->GetHeight()==91880 && pindex->GetBlockHash() == uint256S("0x00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721")));
+
+    // Once BIP34 activated it was not possible to create new duplicate coinbases and thus other than starting
+    // with the 2 existing duplicate coinbase pairs, not possible to create overwriting txs.  But by the
+    // time BIP34 activated, in each of the existing pairs the duplicate coinbase had overwritten the first
+    // before the first had been spent.  Since those coinbases are sufficiently buried its no longer possible to create further
+    // duplicate transactions descending from the known pairs either.
+    // If we're on the known chain at height greater than where BIP34 activated, we can save the db accesses needed for the BIP30 check.
+    if (fEnforceBIP30) {
+        const CBlockIndexView* pindexBIP34height = pindex->GetAncestorView(consensusParams.BIP34Height);
+        //Only continue to enforce if we're below BIP34 activation height or the block hash at that height doesn't correspond.
+        if (!pindexBIP34height || !(pindexBIP34height->GetBlockHash() == consensusParams.BIP34Hash))
+            flags |= TX_VERIFY_BIP30;
+    }
 
     return flags;
 }
@@ -320,6 +348,14 @@ bool Consensus::VerifyTx(const CTransaction& tx, CValidationState& state, const 
     const int64_t nLockTimeCutoff = (flags & LOCKTIME_MEDIAN_TIME_PAST) ? nMedianTimePast : nBlockTime;
     if (!CheckTxPreInputs(tx, state, nHeight, nLockTimeCutoff, nSigOps))
         return false;
+
+    // This could be moved to Consensus::VerifyCoinbaseTx() as an
+    // optimization, but in a strict sense that would be a hardfork
+    if (flags & TX_VERIFY_BIP30) { 
+        const CCoinsInterface* coins = inputs.AccessCoins(tx.GetHash());
+        if (coins && !coins->IsPruned())
+            return state.DoS(100, false, REJECT_INVALID, "bad-txns-BIP30");
+    }
 
     if (tx.IsCoinBase())
         return CheckTxCoinbase(tx, state, flags, nHeight);
