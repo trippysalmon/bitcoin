273e27bd0c086f5ba20cebc2f5ec9e24f9d79015
Jorge Timón <jtimon@jtimon.cc>
2016-07-26 17:49:35 +0200

    Get rid of Consensus::Params::BIP34Height
    If there was a reorg to 227931 (let's hope we don't), bip34 should
    still be activated at height 227931, since we don't have ISM anymore
    to check for an alternative activation height. And if BIP34 is going
    to be activated, then the BIP30 check can be avoided.

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -41,7 +41,6 @@ struct Params {
     int nSubsidyHalvingInterval;
     /** Block height and hash at which BIP34 becomes active */
     int BIP34Height;
-    uint256 BIP34Hash;
     /** Block height at which BIP65 becomes active */
     int BIP65Height;
     /** Block height at which BIP66 becomes active */
