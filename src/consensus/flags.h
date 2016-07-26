f39596a87d6e7366f03576b97d9165cd4326b8ac
Jorge Timón <jtimon@jtimon.cc>
2016-07-27 01:29:54 +0200

    MOVEONLY: Separate consensus/flags.h from script/bitcoinconsensus.h...
    ...to use the same flags internally in bitcoin
    
    TODO: Decouple the bits in consensus flags from the ones in the interpreter flags

---
+++ src/consensus/flags.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CONSENSUS_FLAGS_H
+#define BITCOIN_CONSENSUS_FLAGS_H
+
+/** 
+ * Script verification flags.
+ * @TODO: decouple bit numbers from script flags.
+ */
+enum
+{
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_NONE                = 0,
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_P2SH                = (1U << 0), // evaluate P2SH (BIP16) subscripts
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 2), // enforce strict DER (BIP66) compliance
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9), // enable CHECKLOCKTIMEVERIFY (BIP65)
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10), // enable CHECKSEQUENCEVERIFY (BIP112)
+    bitcoinconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 11), // enable WITNESS (BIP141)
+};
+
+#endif // BITCOIN_CONSENSUS_FLAGS_H
