3f68fd99f8401835ce7df0011de97619ada49832
Jorge Timón <jtimon@jtimon.cc>
2017-02-03 07:59:37 +0100

    Testchains: Introduce attribute blockScript in CBlockHeader and CBlockIndex

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -196,6 +196,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts(boost::function<CBlockIndex*(const uint256
                 pindexNew->nTime          = diskindex.nTime;
                 pindexNew->nBits          = diskindex.nBits;
                 pindexNew->nNonce         = diskindex.nNonce;
+                pindexNew->blockScript    = diskindex.blockScript;
                 pindexNew->nStatus        = diskindex.nStatus;
                 pindexNew->nTx            = diskindex.nTx;
 
