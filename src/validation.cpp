e0ac0dc4b3cd01bb01605926a95d4245c8558077
Jorge Timón <jtimon@jtimon.cc>
2017-02-05 06:54:11 +0100

    Pow: Hide s/CheckProofOfWork/CheckBlockHeader/ in ReadBlockFromDisk

--- src/validation.cpp
+++ src/validation.cpp
@@ -1147,8 +1147,9 @@ bool ReadBlockFromDisk(CBlock& block, const CDiskBlockPos& pos, const Consensus:
     }
 
     // Check the header
-    if (!CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
-        return error("ReadBlockFromDisk: Errors in block header at %s", pos.ToString());
+    CValidationState state;
+    if (!CheckBlockHeader(block, state, consensusParams))
+        return error("%s: CheckBlockHeader: Errors in block header at %s", __func__, pos.ToString());
 
     return true;
 }
